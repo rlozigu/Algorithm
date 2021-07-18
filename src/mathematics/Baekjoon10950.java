1cd08d124d4a9ef9f47e1bea1ea3009470c1fbc5
rlozigu <rlozigu@gmail.com>
2021-07-18 18:54:37 +0900

    BOJ10950: A+B - 3

---
+++ src/mathematics/Baekjoon10950.java
@@ -0,0 +1,21 @@
+package mathematics;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon10950 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        for(int i = 0 ; i < t ; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            System.out.println(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()));
+        }
+
+    }
+}
