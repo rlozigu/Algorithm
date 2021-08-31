8a711f433bfcf087e90a8980512a12d5179341bd
rlozigu <rlozigu@gmail.com>
2021-08-31 20:26:47 +0900

    BOJ10171 - 고양이

---
+++ src/implementation/Baekjoon10171.java
@@ -0,0 +1,10 @@
+package implementation;
+
+public class Baekjoon10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
