c842dc8ce912c662500faf2a5c38284a39cd52b1
rlozigu <rlozigu@gmail.com>
2021-08-31 20:14:08 +0900

    BOJ2743

---
+++ src/implementation/Baekjoon2743.java
@@ -0,0 +1,12 @@
+package implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon2743 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine().length());
+    }
+}
