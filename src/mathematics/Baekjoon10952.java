064119afce53f87ca277ae253446cb98d94cbe4c
rlozigu <rlozigu@gmail.com>
2021-09-07 22:18:07 +0900

    BOJ10952 - A + B -5

---
+++ src/mathematics/Baekjoon10952.java
@@ -0,0 +1,28 @@
+package mathematics;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon10952 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while(true){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            int result = a + b;
+
+            if(result == 0)
+                break;
+
+            sb.append(result).append("\n");
+
+        }
+        System.out.println(sb);
+    }
+}
