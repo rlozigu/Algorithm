8af91daf2777996083648fa4c5bac214116ad496
rlozigu <rlozigu@gmail.com>
2021-09-16 21:50:06 +0900

    BOJ11021 - A + B -7

---
+++ src/mathematics/Baekjoon11021.java
@@ -0,0 +1,26 @@
+package mathematics;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon11021 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int t = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i  <= t ; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            sb.append("Case #").append(i).append(": ").append(a + b).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
