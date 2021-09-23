6e34fa2de80d729366334c2aea323842a9064375
rlozigu <rlozigu@gmail.com>
2021-09-23 23:21:51 +0900

    BOJ2438 - 별찍기 - 1

---
+++ src/io/Baekjoon2438.java
@@ -0,0 +1,23 @@
+package io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon2438 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 1 ; i <= n; i++){
+            for(int j = 0 ; j <i; j++){
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
