bd90cd62ddf901305d6edbe472199e7a228a8159
rlozigu <rlozigu@gmail.com>
2021-09-23 22:57:04 +0900

    BOJ8393 - 합

---
+++ src/io/Baekjoon8393.java
@@ -0,0 +1,15 @@
+package io;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon8393 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+
+        System.out.println(num * (num+1)/2);
+    }
+}
