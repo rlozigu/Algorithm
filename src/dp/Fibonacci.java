9d965b1523ebc982ce74d751bb05c9bfcfc16248
rlozigu <rlozigu@gmail.com>
2021-10-12 22:53:05 +0900

    Fibonacci dp(top-down)

--- src/dp/Fibonacci.java
+++ src/dp/Fibonacci.java
@@ -1,18 +1,26 @@
 package dp;
 
 public class Fibonacci {
+    static int[] dp = new int[100];
+
     public static void main(String[] args) {
-        System.out.println(recur(26));
+        System.out.println(fibo(26));
     }
 
-    public static int recur(int num){
-        if(num == 1){
+    public static int fibo(int num) {
+        //첫번재 수열인 경우
+        if (num == 1) {
             return 1;
         }
-        if(num == 2){
+        //두번째 수열인 경우
+        if (num == 2) {
             return 1;
         }
-        int result = recur(num-1) + recur(num-2);
-        return result;
+        //배열에 저장된 값이 있을 시 저장된 값 반환
+        if (dp[num] != 0) {
+            return dp[num];
+        }
+        //저장된 값이 없는 경우, 값을 저장 후 반환
+        return dp[num] = fibo(num - 1) + fibo(num - 2);
     }
 }
