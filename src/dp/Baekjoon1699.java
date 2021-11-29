9f228ed99326d242474cccd1ba811d139af374f9
rlozigu <rlozigu@gmail.com>
2021-11-29 22:32:43 +0900

    BOJ1699 - 제곱수의 합

---
+++ src/dp/Baekjoon1699.java
@@ -0,0 +1,21 @@
+package dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon1699 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] d = new int[n+1];
+
+        for(int i =1; i <= n; i++){
+            d[i] = i;
+            for(int j = 1; j*j <= i; j++){
+                d[i] = Math.min(d[i], d[i-j*j]+1);
+            }
+        }
+        System.out.println(d[n]);
+    }
+}
