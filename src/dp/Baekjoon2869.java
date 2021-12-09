3cbcc9a286c28a721f86f4f1ff23433518ca57bd
rlozigu <rlozigu@gmail.com>
2021-12-09 22:31:22 +0900

    BOJ2869 - 달팽이는 올라가고 싶다

---
+++ src/dp/Baekjoon2869.java
@@ -0,0 +1,27 @@
+package dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon2869 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
+
+        int day = (v - b) / (a - b);
+
+        // 나머지가 있을 경우 (잔여 블럭이 있을 경우)
+        if ((v - b) % (a - b) != 0) {
+            day++;
+        }
+
+        System.out.println(day);
+
+    }
+}
