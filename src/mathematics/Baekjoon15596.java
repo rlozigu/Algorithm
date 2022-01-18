9bef8532ff7573a14d34fb9b574025f756b575b5
rlozigu <rlozigu@gmail.com>
2022-01-18 23:00:51 +0900

    BOJ15596 - 정수 N개의 합

---
+++ src/mathematics/Baekjoon15596.java
@@ -0,0 +1,13 @@
+package mathematics;
+
+public class Baekjoon15596 {
+    long sum(int[] a) {
+        long sum = 0;	// a 배열 정수 합 변수
+
+        for(int i = 0; i < a.length; i++) {
+            sum += a[i];
+        }
+        return sum;
+    }
+}
+
