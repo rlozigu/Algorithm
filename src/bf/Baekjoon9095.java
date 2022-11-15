a3f496718d43abaeeea7a97e27082da8cd28188d
rlozigu <rlozigu@gmail.com>
2022-11-15 15:59:38 +0900

    BOJ9095 - 1, 2, 3 더하기(재귀)

--- src/bf/Baekjoon9095.java
+++ src/bf/Baekjoon9095.java
@@ -5,64 +5,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Baekjoon9095 {
+    static int go(int sum, int goal){
+        if(sum > goal) return 0;
+        if(sum == goal) return 1;
+
+        int result = 0;
+        result += go(sum + 1, goal);
+        result += go(sum + 2, goal);
+        result += go(sum + 3, goal);
+        return result;
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
 
         while (t-- > 0) {
             int n = Integer.parseInt(br.readLine());
-            int result = 0;
+            int result = go(0, n);
 
-            for (int l1=1; l1<=3; l1++) {
-                if (l1 == n) {
-                    result += 1;
-                }
-                for (int l2=1; l2<=3; l2++) {
-                    if (l1+l2 == n) {
-                        result += 1;
-                    }
-                    for (int l3=1; l3<=3; l3++) {
-                        if (l1+l2+l3 == n) {
-                            result += 1;
-                        }
-                        for (int l4=1; l4<=3; l4++) {
-                            if (l1+l2+l3+l4 == n) {
-                                result += 1;
-                            }
-                            for (int l5=1; l5<=3; l5++) {
-                                if (l1+l2+l3+l4+l5 == n) {
-                                    result += 1;
-                                }
-                                for (int l6=1; l6<=3; l6++) {
-                                    if (l1+l2+l3+l4+l5+l6 == n) {
-                                        result += 1;
-                                    }
-                                    for (int l7=1; l7<=3; l7++) {
-                                        if (l1+l2+l3+l4+l5+l6+l7 == n) {
-                                            result += 1;
-                                        }
-                                        for (int l8=1; l8<=3; l8++) {
-                                            if (l1+l2+l3+l4+l5+l6+l7+l8 == n) {
-                                                result += 1;
-                                            }
-                                            for (int l9=1; l9<=3; l9++) {
-                                                if (l1+l2+l3+l4+l5+l6+l7+l8+l9 == n) {
-                                                    result += 1;
-                                                }
-                                                for (int l0=1; l0<=3; l0++) {
-                                                    if (l1+l2+l3+l4+l5+l6+l7+l8+l9+l0 == n) {
-                                                        result += 1;
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
             System.out.println(result);
         }
     }
