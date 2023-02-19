7a640f254945a1501e328381d9408f2cd3149052
rlozigu <rlozigu@gmail.com>
2023-02-19 15:52:22 +0900

    신규 아이디 추천

---
+++ src/Programmers/P72410.java
@@ -0,0 +1,40 @@
+package Programmers;
+
+public class P72410 {
+    public static void main(String[] args) {
+        System.out.println(solution("123_.def"));
+    }
+
+    static String solution(String new_id) {
+        //1단계
+        new_id = new_id.toLowerCase();
+        //2단계
+        new_id = new_id.replaceAll("[^a-z0-9-_.]", "");
+        //3단계
+        new_id = new_id.replaceAll("\\.+", ".");
+        //4단계
+        if (new_id.length() > 0 && new_id.charAt(0) == '.') {
+            new_id = new_id.substring(1, new_id.length());
+        }
+        if (new_id.length() > 0 && new_id.charAt(new_id.length() - 1) == '.') {
+            new_id = new_id.substring(0, new_id.length() - 1);
+        }
+        //5단계
+        if ("".equals(new_id)) {
+            new_id = "a";
+        }
+        //6단계
+        if(new_id.length() > 15){
+            new_id = new_id.substring(0, 15);
+            if (new_id.charAt(new_id.length() - 1) == '.') {
+                new_id = new_id.substring(0, new_id.length() - 1);
+            }
+        }
+        //7단계
+        while (new_id.length() < 3 ) {
+            new_id += new_id.charAt(new_id.length() - 1);
+        }
+
+        return new_id;
+    }
+}
