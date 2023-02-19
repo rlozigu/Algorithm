b9677c6fa5d4d3c4beb77c011c640c785ff1f036
rlozigu <rlozigu@gmail.com>
2023-02-19 15:20:43 +0900

    숫자 문자열과 영단어 - 수정

--- src/Programmers/P81301.java
+++ src/Programmers/P81301.java
@@ -6,77 +6,13 @@ public class P81301 {
     }
 
     static int solution(String s) {
-        String result = "";
-        char[] arr = s.toCharArray();
+        String[] digits = {"0","1","2","3","4","5","6","7","8","9"};
+        String[] alphabets = {"zero","one","two","three","four","five","six","seven","eight","nine"};
 
-        for (int i = 0; i < arr.length; i++) {
-            char c = arr[i];
-
-            if (48 <= c && c <= 57) {
-                result += c;
-                continue;
-            }
-            switch (c) {
-                case 'z':
-                    result += "0";
-                    i += 3;
-                    break;
-                case 'o':
-                    result += "1";
-                    i += 2;
-                    break;
-                case 't':
-                    switch (arr[i + 1]) {
-                        case 'w' : {
-                            result += "2";
-                            i += 2;
-                            break;
-                        }
-                        case 'h' : {
-                            result += "3";
-                            i += 4;
-                            break;
-                        }
-                    }
-                    break;
-                case 'f':
-                    switch (arr[i + 1]) {
-                        case 'o' : {
-                            result += "4";
-                            i += 3;
-                            break;
-                        }
-                        case 'i' : {
-                            result += "5";
-                            i += 3;
-                            break;
-                        }
-                    }
-                    break;
-                case 's':
-                    switch (arr[i + 1]) {
-                        case 'i' : {
-                            result += "6";
-                            i += 2;
-                            break;
-                        }
-                        case 'e' : {
-                            result += "7";
-                            i += 4;
-                            break;
-                        }
-                    }
-                    break;
-                case 'e':
-                    result += "8";
-                    i += 4;
-                    break;
-                case 'n':
-                    result += "9";
-                    i += 3;
-                    break;
-            }
+        for (int i = 0; i < 10; i++) {
+            s = s.replaceAll(alphabets[i], digits[i]);
         }
-        return Integer.parseInt(result);
+
+        return Integer.parseInt(s);
     }
 }
